8c78244d492a2be74d03b48865fea5f97e1c0c4f
cmmerritt <christiane.merritt@gmail.com>
2021-06-15 10:32:21 -0700

    echo post test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,4 +6,9 @@ describe('app routes', () => {
     const res = await request(app).get('/');
     expect(res.text).toEqual('hi');
   });
+  it('POST request to /echo responds with status code 200 and plain text with request body', async() => {
+    const res = await request(app).post('/echo').send('oh hai Mark');
+    expect(res.text).toEqual('oh hai Mark');
+    expect(res.status).toEqual(200);
+  });
 });
