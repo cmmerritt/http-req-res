ccc1b62d6373118fd6023be8dc3e7f042af909ce
cmmerritt <christiane.merritt@gmail.com>
2021-06-14 23:29:29 -0700

    get request to / test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-
+  it('GET request to / responds with plain text hi', async() => {
+    const res = await request(app).get('/');
+    expect(res.text).toEqual('hi');
+  });
 });
