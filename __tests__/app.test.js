88731e3199b505211609520d74b38f9945feb99d
cmmerritt <christiane.merritt@gmail.com>
2021-06-15 10:47:33 -0700

    get blue route passes test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,10 +13,14 @@ describe('app routes', () => {
   });
   it('GET request to /red responds with html with h1 and word red', async() => {
     const res = await request(app).get('/red');
-    expect(res.text).toEqual('<h1>red<h1>');
+    expect(res.text).toEqual('<h1>red</h1>');
   });
   it('GET request to /green responds with html with h1 and word green', async() => {
     const res = await request(app).get('/green');
-    expect(res.text).toEqual('<h1>green<h1>');
+    expect(res.text).toEqual('<h1>green</h1>');
+  });
+  it('GET request to /blue responds with html with h1 and word blue', async() => {
+    const res = await request(app).get('/blue');
+    expect(res.text).toEqual('<h1>blue</h1>');
   });
 });
